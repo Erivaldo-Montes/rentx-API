1fe7c28de5fd8b35db32fe56c2b5d412219bfbed
Erivaldo Montes <erivaldomontez@gmail.com>
2022-08-04 17:45:44 -0300

    refactor controllers

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,23 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import { createCategoryController } from '../modules/cars/useCases/createCategory';
-import { importCategoriesController } from '../modules/cars/useCases/importCategories/index';
-import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import createCategoryController from '../modules/cars/useCases/createCategory';
+import importCategoriesController from '../modules/cars/useCases/importCategories/index';
+import listCategoriesController from '../modules/cars/useCases/listCategories';
 
 const upload = multer({ dest: './tmp' });
 
 const categoriesRoutes = Router();
 
 categoriesRoutes.post('/', (request, response) => {
-  return createCategoryController.handle(request, response);
+  return createCategoryController().handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
-  return listCategoriesController.handle(request, response);
+  return listCategoriesController().handle(request, response);
 });
 
 categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
-  return importCategoriesController.handle(request, response);
+  return importCategoriesController().handle(request, response);
 });
 export { categoriesRoutes };
