a26e092110122615383d21a8fa46e3401acb2e6f
Erivaldo Montes <erivaldomontez@gmail.com>
2022-08-07 11:03:06 -0300

    fix status code

--- src/modules/cars/useCases/importCategories/importCategoriesController.ts
+++ src/modules/cars/useCases/importCategories/importCategoriesController.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express';
-import { container } from 'tsyringe';
+import { Request, Response } from "express";
+import { container } from "tsyringe";
 
-import { ImportCategoriesUseCase } from './importCategoriesUseCase';
+import { ImportCategoriesUseCase } from "./importCategoriesUseCase";
 
 class ImportCategoriesController {
   async handle(request: Request, response: Response): Promise<Response> {
@@ -10,7 +10,7 @@ class ImportCategoriesController {
     const importCategoriesUseCase = container.resolve(ImportCategoriesUseCase);
 
     await importCategoriesUseCase.execute(file);
-    return response.status(200).send();
+    return response.status(201).send();
   }
 }
 
