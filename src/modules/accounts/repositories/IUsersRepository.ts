c4947eacb15b10ea6b29231de4008786a0c63311
Erivaldo Montes <erivaldomontez@gmail.com>
2022-08-10 09:22:30 -0300

    add findByName function

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,7 +1,9 @@
 import { ICreateUserDTO } from "../DTOs/ICreateUser";
+import { User } from "../entities/user";
 
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
 
 export { IUsersRepository };
