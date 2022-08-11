4d1543c5ff4291296465c04d192b6225ec95320a
Erivaldo Montes <erivaldomontez@gmail.com>
2022-08-11 11:13:18 -0300

    add double quotes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,11 @@
-import { Router } from 'express';
-import multer from 'multer';
+import { Router } from "express";
+import multer from "multer";
 
-import { CreateCategoryController } from '../modules/cars/useCases/createCategory/createCategoryController';
-import { ImportCategoriesController } from '../modules/cars/useCases/importCategories/importCategoriesController';
-import { ListCategoriesController } from '../modules/cars/useCases/listCategories/listCategoriesController';
+import { CreateCategoryController } from "../modules/cars/useCases/createCategory/createCategoryController";
+import { ImportCategoriesController } from "../modules/cars/useCases/importCategories/importCategoriesController";
+import { ListCategoriesController } from "../modules/cars/useCases/listCategories/listCategoriesController";
 
-const upload = multer({ dest: './tmp' });
+const upload = multer({ dest: "./tmp" });
 
 const categoriesRoutes = Router();
 
@@ -13,13 +13,13 @@ const createCategoryController = new CreateCategoryController();
 const importCategoriesController = new ImportCategoriesController();
 const listCategoriesController = new ListCategoriesController();
 
-categoriesRoutes.post('/', createCategoryController.handle);
+categoriesRoutes.post("/", createCategoryController.handle);
 
-categoriesRoutes.get('/', listCategoriesController.handle);
+categoriesRoutes.get("/", listCategoriesController.handle);
 
 categoriesRoutes.post(
-  '/import',
-  upload.single('file'),
+  "/import",
+  upload.single("file"),
   importCategoriesController.handle,
 );
 export { categoriesRoutes };
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,13 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/createSpecificationController';
+import { ensureAutheticate } from "../middleware/ensureAuthenticate";
+import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/createSpecificationController";
 
 const specificationRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
 specificationRoutes.use(ensureAutheticate);
+specificationRoutes.post("/", createSpecificationController.handle);
 
 export { specificationRoutes };
