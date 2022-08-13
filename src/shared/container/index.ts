57c3e88a85378ccfb4edb48f9259baacdb25fddc
Erivaldo Montes <erivaldomontez@gmail.com>
2022-08-13 09:30:00 -0300

    add paths alias

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,11 +1,11 @@
 import { container } from "tsyringe";
 
-import { UsersRepository } from "../../modules/accounts/repositories/implementations/usersRepository";
-import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
-import { ICategoryRepository } from "../../modules/cars/repositories/ICategoriesRepository";
-import { CategoriesRepository } from "../../modules/cars/repositories/implementations/categoriesRepository";
-import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/specificationsRepository";
-import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+import { UsersRepository } from "@modules/accounts/repositories/implementations/usersRepository";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { ICategoryRepository } from "@modules/cars/repositories/ICategoriesRepository";
+import { CategoriesRepository } from "@modules/cars/repositories/implementations/categoriesRepository";
+import { SpecificationsRepository } from "@modules/cars/repositories/implementations/specificationsRepository";
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
 container.registerSingleton<ICategoryRepository>(
   "CategoriesRepository",
