5406fcf2b73e8b4f9f8f26f356be45504b2dbd57
Erivaldo Montes <erivaldomontez@gmail.com>
2022-08-20 15:34:12 -0300

    add routes specifications to cars

--- src/modules/cars/useCases/createCarSpecification/createCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecification/createCarSpecificationUseCase.ts
@@ -1,3 +1,6 @@
+import { inject, injectable } from "tsyringe";
+
+import { Car } from "@modules/cars/infra/typeorm/entities/car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 import { AppError } from "@shared/errors/appError";
@@ -6,13 +9,18 @@ interface IRequest {
   car_id: string;
   specifications_id: string[];
 }
+
+@injectable()
 class CreateCarSpecificationUseCase {
   constructor(
+    @inject("CarsRepository")
     private carsRepository: ICarsRepository,
+
+    @inject("SpecificationsRepository")
     private specificationRepository: ISpecificationsRepository,
   ) {}
 
-  async execute({ car_id, specifications_id }: IRequest): Promise<void> {
+  async execute({ car_id, specifications_id }: IRequest): Promise<Car> {
     const carExist = await this.carsRepository.findById(car_id);
 
     if (!carExist) {
@@ -27,7 +35,7 @@ class CreateCarSpecificationUseCase {
 
     await this.carsRepository.create(carExist);
 
-    console.log(carExist);
+    return carExist;
   }
 }
 
--- src/shared/infra/HTTP/routes/cars.routes.ts
+++ src/shared/infra/HTTP/routes/cars.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/createCarController";
+import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/createCarSpecificationsController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/listAvailableCarsController";
 
 import { ensureAdmin } from "../middleware/ensureAdmin";
@@ -10,9 +11,17 @@ const carRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
 carRoutes.post("/", ensureAutheticate, ensureAdmin, createCarController.handle);
 
 carRoutes.get("/available", listAvailableCarsController.handle);
+
+carRoutes.post(
+  "/specifications/:id",
+  ensureAutheticate,
+  ensureAdmin,
+  createCarSpecificationController.handle,
+);
 
 export { carRoutes };
