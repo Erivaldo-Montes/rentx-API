08e01f67e3814d8587169c2f7b86da0c74a0fa7f
Erivaldo Montes <erivaldomontez@gmail.com>
2022-08-20 15:36:12 -0300

    add findByids method

--- src/modules/cars/infra/typeorm/repositories/specificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/specificationsRepository.ts
@@ -1,12 +1,11 @@
 import { Repository } from "typeorm";
 
 import { Specification } from "@modules/cars/infra/typeorm/entities/specification";
-import appDataSource from "@shared/infra/typeorm/index";
-
 import {
   ICreateSpecification,
   ISpecificationsRepository,
-} from "../../../repositories/ISpecificationsRepository";
+} from "@modules/cars/repositories/ISpecificationsRepository";
+import appDataSource from "@shared/infra/typeorm/index";
 
 class SpecificationsRepository implements ISpecificationsRepository {
   private repository: Repository<Specification>;
@@ -14,19 +13,35 @@ class SpecificationsRepository implements ISpecificationsRepository {
   constructor() {
     this.repository = appDataSource.getRepository(Specification);
   }
-  async create({ name, description }: ICreateSpecification): Promise<void> {
+
+  async create({
+    name,
+    description,
+  }: ICreateSpecification): Promise<Specification> {
     const specification = this.repository.create({
       name,
       description,
     });
 
     await this.repository.save(specification);
+
+    return specification;
   }
 
   async findByName(name: string): Promise<Specification> {
     const specification = await this.repository.findOne({ where: { name } });
 
     return specification;
+  }
+
+  async findByIds(ids: string[]): Promise<Specification[]> {
+    const specificationsArray = await this.repository.find();
+
+    const specifications = specificationsArray.filter(specification => {
+      const re = ids.find(id => specification.id === id);
+      return re;
+    });
+    return specifications;
   }
 }
 
