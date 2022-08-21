af4284ebe00f92f14cfa055423b68c38790d748f
Erivaldo Montes <erivaldomontez@gmail.com>
2022-08-21 17:08:42 -0300

    register respository container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,9 +2,11 @@ import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/infra/typorm/repositories/usersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { CarImagesRepository } from "@modules/cars/infra/typeorm/repositories/carImagesRepository";
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/carsRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/categoriesRepository";
 import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/specificationsRepository";
+import { ICarImagesRepository } from "@modules/cars/repositories/ICarImagesRepository";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ICategoryRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
@@ -25,3 +27,8 @@ container.registerSingleton<IUsersRepository>(
 );
 
 container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
+
+container.registerSingleton<ICarImagesRepository>(
+  "CarImagesRespository",
+  CarImagesRepository,
+);
