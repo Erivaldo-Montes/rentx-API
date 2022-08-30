d5290c6d103bcf8337b0d8a9fcdeab020d990807
Erivaldo Montes <erivaldomontez@gmail.com>
2022-08-30 20:48:37 -0300

    fix bug

--- src/shared/container/providers/dateProvider/IDateProvider.ts
+++ src/shared/container/providers/dateProvider/IDateProvider.ts
@@ -2,6 +2,7 @@ interface IDateProvider {
   compareInHours(start_date: Date, end_date: Date): number;
   dateNow(): Date;
   convertToUtc(date: Date): string;
+  compareInDays(start_date: Date, end_date: Date): number;
 }
 
 export { IDateProvider };
