a69c3d7f4f8361368641e5c693114164c0745824
Erivaldo Montes <erivaldomontez@gmail.com>
2022-09-03 12:43:32 -0300

    show entity relations

--- src/modules/rentals/infra/typeorm/entities/rental.ts
+++ src/modules/rentals/infra/typeorm/entities/rental.ts
@@ -1,10 +1,23 @@
 import { randomUUID } from "crypto";
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
+
+import { Car } from "@modules/cars/infra/typeorm/entities/car";
 
 @Entity("rentals")
 class Rental {
   @PrimaryColumn()
   id?: string;
+
+  @ManyToOne(() => Car)
+  @JoinColumn({ name: "car_id" })
+  car: Car;
 
   @Column()
   car_id: string;
