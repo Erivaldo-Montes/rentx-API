c29347b075f33071ed59b00e666eda74a950afce
Erivaldo Montes <erivaldomontez@gmail.com>
2022-09-07 19:14:17 -0300

    data provider

--- src/shared/container/providers/dateProvider/IDateProvider.ts
+++ src/shared/container/providers/dateProvider/IDateProvider.ts
@@ -3,6 +3,8 @@ interface IDateProvider {
   dateNow(): Date;
   convertToUtc(date: Date): string;
   compareInDays(start_date: Date, end_date: Date): number;
+  addDays(days: number): Date;
+  addHours(hours: number): Date;
 }
 
 export { IDateProvider };
