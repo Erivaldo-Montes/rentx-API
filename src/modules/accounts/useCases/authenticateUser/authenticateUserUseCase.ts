b3bb01df4767def050184fdd646c57126fe41a66
Erivaldo Montes <erivaldomontez@gmail.com>
2022-09-07 19:03:00 -0300

    refresh token

--- src/modules/accounts/useCases/authenticateUser/authenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/authenticateUserUseCase.ts
@@ -2,7 +2,10 @@ import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import auth from "@config/auth";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { IUsersTokenRepository } from "@modules/accounts/repositories/IUsersTokenRepository";
+import { IDateProvider } from "@shared/container/providers/dateProvider/IDateProvider";
 import { AppError } from "@shared/errors/appError";
 
 interface IRequest {
@@ -16,6 +19,7 @@ interface IResponse {
     name: string;
   };
   token: string;
+  refresh_token: string;
 }
 
 @injectable()
@@ -23,9 +27,22 @@ class AuthenticateUserUseCase {
   constructor(
     @inject("UsersRepository")
     private userRepository: IUsersRepository,
+
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokenRepository,
+
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider,
   ) {}
   async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.userRepository.findByEmail(email);
+    const {
+      expires_in_token,
+      secret_refresh_token,
+      secret_token,
+      expires_in_refresh_token,
+      expires_refresh_token_days,
+    } = auth;
 
     if (!user) {
       throw new AppError("email or password incorrect");
@@ -37,13 +54,28 @@ class AuthenticateUserUseCase {
       throw new AppError("email or password incorrect");
     }
 
-    const token = sign({}, "jesus", {
+    const token = sign({}, secret_token, {
       subject: user.id,
-      expiresIn: "1d",
+      expiresIn: expires_in_token,
+    });
+
+    const refresh_token_expires_date = this.dateProvider.addDays(
+      expires_refresh_token_days,
+    );
+
+    const refresh_token = sign({ email }, secret_refresh_token, {
+      subject: user.id,
+      expiresIn: expires_in_refresh_token,
+    });
+    await this.usersTokensRepository.create({
+      expires_date: refresh_token_expires_date,
+      refresh_token,
+      user_id: user.id,
     });
 
     const tokenReturn: IResponse = {
       token,
+      refresh_token,
       user: {
         email: user.email,
         name: user.name,
---
+++ src/modules/accounts/useCases/refreshToken/refreshTokenUseCase.ts
@@ -0,0 +1,58 @@
+import { verify, sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import auth from "@config/auth";
+import { IUsersTokenRepository } from "@modules/accounts/repositories/IUsersTokenRepository";
+import { IDateProvider } from "@shared/container/providers/dateProvider/IDateProvider";
+import { AppError } from "@shared/errors/appError";
+
+interface IPayload {
+  sub: string;
+  email: string;
+}
+
+@injectable()
+class RefreshTokenUseCase {
+  constructor(
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokenRepository,
+
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider,
+  ) {}
+  async execute(token: string) {
+    const { sub, email } = verify(token, auth.secret_refresh_token) as IPayload;
+    const user_id = sub;
+
+    const userToken =
+      await this.usersTokensRepository.findByUserIdAndRefreshToken(
+        user_id,
+        token,
+      );
+
+    if (!userToken) {
+      throw new AppError("refresh token does not exists");
+    }
+
+    await this.usersTokensRepository.deleteById(userToken.id);
+
+    const refresh_token = sign({ email }, auth.secret_refresh_token, {
+      subject: user_id,
+      expiresIn: auth.expires_in_refresh_token,
+    });
+
+    const expires_date = this.dateProvider.addDays(
+      auth.expires_refresh_token_days,
+    );
+
+    await this.usersTokensRepository.create({
+      expires_date,
+      user_id,
+      refresh_token,
+    });
+
+    return refresh_token;
+  }
+}
+
+export { RefreshTokenUseCase };
