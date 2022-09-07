76ca486809e1a386748dd15725bb1edb008d4b3f
Erivaldo Montes <erivaldomontez@gmail.com>
2022-09-07 19:14:42 -0300

    data provider injection

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,8 +2,15 @@ import { container } from "tsyringe";
 
 import { IDateProvider } from "./dateProvider/IDateProvider";
 import { DayjsDateProvider } from "./dateProvider/implementations/dayjsDateProvider";
+import { IMailProvider } from "./MailProvider/IMailProvider";
+import { EtherealMailProvider } from "./MailProvider/implementation/etherealMailProvider";
 
 container.registerSingleton<IDateProvider>(
   "DayjsDateProvider",
   DayjsDateProvider,
+);
+
+container.registerInstance<IMailProvider>(
+  "EtherealMailProvider",
+  new EtherealMailProvider(),
 );
