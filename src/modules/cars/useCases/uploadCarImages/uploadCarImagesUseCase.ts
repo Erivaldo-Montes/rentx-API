a9c625048c7bd0709a21d34e5986a0e91db5a9a5
Erivaldo Montes <erivaldomontez@gmail.com>
2022-09-17 15:01:37 -0300

    car imagems storage

--- src/modules/cars/useCases/uploadCarImages/uploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/uploadCarImagesUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
 import { ICarImagesRepository } from "@modules/cars/repositories/ICarImagesRepository";
+import { IStorageProvider } from "@shared/container/providers/storageProvider/IStorageProvider";
 
 interface IRequest {
   car_id: string;
@@ -12,11 +13,15 @@ class UploadCarImagesUseCase {
   constructor(
     @inject("CarImagesRespository")
     private carImageRepository: ICarImagesRepository,
+
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider,
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
     images_name.map(async image => {
       await this.carImageRepository.create(car_id, image);
+      await this.storageProvider.save(image, "cars");
     });
   }
 }
