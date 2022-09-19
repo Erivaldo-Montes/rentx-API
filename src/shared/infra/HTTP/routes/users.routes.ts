4b60293f60524593e8691d18a8fafa14d3a82409
Erivaldo Montes <erivaldomontez@gmail.com>
2022-09-18 22:09:06 -0300

    user profile

--- src/shared/infra/HTTP/routes/users.routes.ts
+++ src/shared/infra/HTTP/routes/users.routes.ts
@@ -4,6 +4,7 @@ import multer from "multer";
 import uploadConfig from "@config/upload";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/createUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/updateUserAvatarController";
+import { UserProfileController } from "@modules/accounts/useCases/userProfile/userProfileController";
 import { ensureAutheticate } from "@shared/infra/HTTP/middleware/ensureAuthenticate";
 
 const userRoutes = Router();
@@ -11,6 +12,7 @@ const userRoutes = Router();
 const uploadAvatar = multer(uploadConfig);
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const userProfileController = new UserProfileController();
 
 userRoutes.post("/", createUserController.handle);
 
@@ -20,5 +22,7 @@ userRoutes.patch(
   uploadAvatar.single("avatar"),
   updateUserAvatarController.handle,
 );
+
+userRoutes.get("/profile", ensureAutheticate, userProfileController.handle);
 
 export { userRoutes };
